06e40d3801b805388f982b58bc21cf895c911eb1
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-12-05 09:52:40 +0300

    pathway reaction enrichment algorithm added

---
+++ metabolitics/preprocessing/pathway_reaction_enrichment.py
@@ -0,0 +1,28 @@
+import cobra as cb
+from functional import seq
+from sklearn_utils.preprocessing import FunctionalEnrichmentAnalysis
+
+import metabolitics.extensions
+from metabolitics.utils import load_network_model
+
+
+class PathwayReactionEnrichment(FunctionalEnrichmentAnalysis):
+    """Functional Enrichment Analysis"""
+
+    def __init__(self,
+                 reference_label='healthy',
+                 network_model='recon2',
+                 **kwargs):
+        '''
+        :reference_label: label of refence values in the calculation
+        :dataset_name: name of metabolitics network
+        '''
+        model = load_network_model(network_model)
+
+        feature_groups = seq(model.reactions) \
+                         .map(lambda r: (r.subsystem, '%s_dif' % r.id)) \
+                         .filter(lambda x: not cb.Model.is_transport_subsystem(x[0])) \
+                         .group_by_key() \
+                         .to_dict()
+
+        super().__init__(reference_label, feature_groups, **kwargs)
